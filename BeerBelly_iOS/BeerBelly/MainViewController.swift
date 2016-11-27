a97c706f159352a278ef0572c5939c988bd9414d
Soo Park <soorin1993@gmail.com>
2016-11-27 15:46:10 -0700

    ios setip

---
+++ BeerBelly_iOS/BeerBelly/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BeerBelly
+//
+//  Created by Soo Rin Park on 11/17/16.
+//  Copyright © 2016 Soo Rin Park. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
